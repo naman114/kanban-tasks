5950f9a249d3de1e7cf77e79d3f0124b7b386c32
Naman Gogia <namangogia2001@gmail.com>
2022-04-26 04:16:06 +0530

    Add homepage UI

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -4,47 +4,95 @@ import Sidebar from "./Sidebar";
 
 export default function Home() {
   return (
-    <div className="flex h-screen overflow-hidden">
+    <div className="flex h-screen">
       <Sidebar />
 
       <Content>
-        <div className="flex min-h-full flex-col space-y-4 px-4 sm:px-6 lg:px-8">
+        <div className="flex h-screen flex-col space-y-4 px-16 py-6">
+          <div className="mb-8 flex flex-col gap-1">
+            <p className="text-[16px] text-slate-900">Tuesday, March 1</p>
+            <p className="text-[32px] text-slate-900">Good Evening Jerin</p>
+          </div>
           <div className="grid grid-cols-3 gap-4">
-            <div className="mx-2 h-28 cursor-pointer rounded-xl bg-white hover:bg-slate-100">
-              <a href="/patient/{{ patient.id }}/">
-                <div className="mx-8 my-3 flex">
-                  <div className="flex flex-col">
-                    <p>Incomplete Tasks</p>
-                    <p className="text-gray-400">0</p>
-                    <p className="text-gray-400">Task Count</p>
-                  </div>
+            <div className="rounded border-2 bg-white">
+              <div className="mx-8 my-3 flex">
+                <div className="flex flex-col">
+                  <p className="mb-8 text-slate-900">Completed Tasks</p>
+                  <p className="text-lg font-bold text-slate-900">0</p>
+                  <p className="text-zinc-500">Task Count</p>
                 </div>
-              </a>
+              </div>
             </div>
-            <div className="mx-2 h-28 cursor-pointer rounded-xl bg-white hover:bg-slate-100">
-              <a href="/patient/{{ patient.id }}/">
-                <div className="mx-8 my-3 flex">
-                  <div className="flex flex-col">
-                    <p>Completed Tasks</p>
-                    <p className="text-gray-400">0</p>
-                    <p className="text-gray-400">Task Count</p>
-                  </div>
+            <div className="rounded border-2 bg-white">
+              <div className="mx-8 my-3 flex">
+                <div className="flex flex-col">
+                  <p className="mb-8 text-slate-900">Incomplete Tasks</p>
+                  <p className="text-lg font-bold text-slate-900">0</p>
+                  <p className="text-zinc-500">Task Count</p>
                 </div>
-              </a>
+              </div>
             </div>
-            <div className="mx-2 h-28 cursor-pointer rounded-xl bg-white hover:bg-slate-100">
-              <a href="/patient/{{ patient.id }}/">
-                <div className="mx-8 my-3 flex">
-                  <div className="flex flex-col">
-                    <p>Total Tasks</p>
-                    <p className="text-gray-400">0</p>
-                    <p className="text-gray-400">Task Count</p>
-                  </div>
+            <div className="rounded border-2 bg-white">
+              <div className="mx-8 my-3 flex">
+                <div className="flex flex-col">
+                  <p className="mb-8 text-slate-900">Total Tasks</p>
+                  <p className="text-lg font-bold text-slate-900">0</p>
+                  <p className="text-zinc-500">Task Count</p>
                 </div>
-              </a>
+              </div>
             </div>
           </div>
-          <p>My Tasks</p>
+          <h1 className="pt-10 text-2xl font-medium text-slate-900">
+            My Tasks
+          </h1>
+          <div className="flex w-full gap-4 border-b-2">
+            <button className="mb-[-2px] border-b-4 border-zinc-500 px-3 pt-2 font-bold text-zinc-500">
+              Todo
+            </button>
+            <button className="px-3 pt-2 font-medium text-zinc-500">
+              On Progress
+            </button>
+            <button className="px-3 pt-2 font-medium text-zinc-500">
+              Done
+            </button>
+          </div>
+          <div className="mb-2 flex max-h-56 flex-col gap-2 overflow-scroll pt-3">
+            <div className="w-full rounded bg-stone-200">
+              <p className="px-8 py-4 font-medium text-zinc-500">
+                Updating Design System
+              </p>
+            </div>
+            <div className="w-full rounded bg-stone-200">
+              <p className="px-8 py-4 font-medium text-zinc-500">
+                Updating Design System
+              </p>
+            </div>
+            <div className="w-full rounded bg-stone-200">
+              <p className="px-8 py-4 font-medium text-zinc-500">
+                Updating Design System
+              </p>
+            </div>
+            <div className="w-full rounded bg-stone-200">
+              <p className="px-8 py-4 font-medium text-zinc-500">
+                Updating Design System
+              </p>
+            </div>
+            <div className="w-full rounded bg-stone-200">
+              <p className="px-8 py-4 font-medium text-zinc-500">
+                Updating Design System
+              </p>
+            </div>
+            <div className="w-full rounded bg-stone-200">
+              <p className="px-8 py-4 font-medium text-zinc-500">
+                Updating Design System
+              </p>
+            </div>
+            <div className="w-full rounded bg-stone-200">
+              <p className="px-8 py-4 font-medium text-zinc-500">
+                Updating Design System
+              </p>
+            </div>
+          </div>
         </div>
       </Content>
     </div>
